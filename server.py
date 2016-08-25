05b7ad032e7f8855dbdc853750ea9fa79ccf1049
Marc Christie <mchristie@walkerinfo.com>
2016-08-25 11:05:40 -0400

    optimistically updating

--- server.py
+++ server.py
@@ -12,7 +12,7 @@
 import json
 import os
 import time
-from flask import Flask, Response, request
+from flask import Flask, Response, request, abort
 
 app = Flask(__name__, static_url_path='', static_folder='public')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
@@ -24,6 +24,7 @@ def comments_handler():
         comments = json.loads(f.read())
 
     if request.method == 'POST':
+        #abort(500) # for testing optimistically updating
         new_comment = request.form.to_dict()
         new_comment['id'] = int(time.time() * 1000)
         comments.append(new_comment)
